be732890ba16b6963ecb5032a90da27564929719
Duncan Davidson <duncandavidson@me.com>
2021-01-19 23:42:19 +0100

    Evolve forward

--- lib/content.ts
+++ lib/content.ts
@@ -8,50 +8,75 @@ import html from 'remark-html'
 import yaml from 'yaml'
 
 export interface ContentProps {
-  title: string
-  date: string
   html: string
   meta: {}
+  title?: string
+  date?: string
+  emoji?: string
 }
 
 export class Content {
-  source: string
-  slug: string
-  text: string
+  filename: string
+  rawText: string
   html: string
   meta: {}
 
-  constructor(source: string, slug?: string) {
-    this.source = source
+  constructor(filename: string) {
+    this.filename = filename
 
-    if (slug) {
-      this.slug = slug
-    } else {
-      this.slug = path.dirname(source) + '/' + path.basename(source, '.md')
-    }
-
-    this.text = fs.readFileSync(source, 'utf8')
+    this.rawText = fs.readFileSync(filename, 'utf8')
 
     let doc = unified()
-      .use(markdown)
       .use(frontmatter)
       .use(extract, { yaml: yaml.parse })
+      .use(markdown)
       .use(html)
-      .processSync(this.text)
+      .processSync(this.rawText)
 
     this.html = String(doc.contents)
     this.meta = doc.data
   }
 
   get type(): string {
-    return this.meta['type']
+    if (this.meta['type']) {
+      return this.meta['type']
+    } else {
+      return 'text'
+    }
   }
 
   get title(): string {
-    return this.meta['title']
+    if (this.meta['title'] != undefined) {
+      return this.meta['title']
+    } else {
+      return path.basename(this.filename, path.extname(this.filename))
+    }
   }
 
   get emoji(): string {
     return this.meta['emoji']
+  }
+
+  get date(): string {
+    return this.meta['date']
+  }
+
+  get props(): ContentProps {
+    let props = {
+      html: this.html,
+      meta: this.meta,
+    }
+
+    if (this.title != undefined) {
+      props['title'] = this.title
+    }
+    if (this.date != undefined) {
+      props['date'] = this.date
+    }
+    if (this.emoji != undefined) {
+      props['emoji'] = this.emoji
+    }
+
+    return props
   }
 }
--- tests/content-tests.ts
+++ tests/content-tests.ts
@@ -4,47 +4,75 @@ describe('Content', () => {
   let linkContent = new Content('test-content/link.md')
   let noMetaContent = new Content('test-content/no-meta.md')
 
-  test('source path from link.md', () => {
-    expect(linkContent.source).toBe('test-content/link.md')
+  describe('filename', () => {
+    test('filename is set', () => {
+      expect(linkContent.filename).toBe('test-content/link.md')
+    })
   })
 
-  test('slug from link.md', () => {
-    expect(linkContent.slug).toBe('test-content/link')
+  describe('metadata', () => {
+    test('type from link.md', () => {
+      expect(linkContent.type).toBe('link')
+    })
+
+    test('type from no-meta.md', () => {
+      expect(noMetaContent.type).toBe('text')
+    })
+
+    test('title from link.md', () => {
+      expect(linkContent.title).toBe('Link title')
+    })
+
+    test('title from no-meta.md', () => {
+      expect(noMetaContent.title).toBe('no-meta')
+    })
+
+    test('emoji from link.md', () => {
+      expect(linkContent.emoji).toContain('🤦‍♂️')
+    })
+
+    test('emoji from no-meta.md', () => {
+      expect(noMetaContent.emoji).toBeNull
+    })
   })
 
-  test('type from link.md', () => {
-    expect(linkContent.type).toBe('link')
+  describe('content', () => {
+    test('raw text from link.md', () => {
+      expect(linkContent.rawText).toContain('title: Link title')
+    })
+
+    test('raw text from no-meta.md', () => {
+      expect(noMetaContent.rawText).toContain('This is some markdown')
+    })
+
+    test('html from link.md', () => {
+      expect(linkContent.html).toContain('<p>Text <a href="https:')
+    })
+
+    test('html from no-meta.md', () => {
+      expect(noMetaContent.html).toContain('<p>This is some markdown')
+    })
   })
 
-  test('raw text from link.md', () => {
-    expect(linkContent.text).toContain('title: Link title')
-  })
+  describe('props', () => {
+    test('props from link.md', () => {
+      let props = linkContent.props
+      expect(props.title).toBe('Link title')
+      expect(props.html).toContain('<p>Text <a href="https:')
+      expect(props.date).toEqual('2021-01-19')
+      expect(props.emoji).toBe('🤦‍♂️')
+      expect(props.meta['emoji']).toBe('🤦‍♂️')
+      expect(props.meta['title']).toBe('Link title')
+      expect(props.meta['type']).toBe('link')
+    })
 
-  test('raw text from no-meta.md', () => {
-    expect(noMetaContent.text).toContain('This is some markdown')
-  })
-
-  test('title from link.md', () => {
-    expect(linkContent.title).toBe('Link title')
-  })
-
-  test('title from no-meta.md', () => {
-    expect(noMetaContent.title).toBeNull
-  })
-
-  test('html from link.md', () => {
-    expect(linkContent.html).toContain('<p>Text <a href="https:')
-  })
-
-  test('html from no-meta.md', () => {
-    expect(noMetaContent.html).toContain('<p>This is some markdown')
-  })
-
-  test('emoji from link.md', () => {
-    expect(linkContent.emoji).toContain('🤦‍♂️')
-  })
-
-  test('emoji from no-meta.md', () => {
-    expect(noMetaContent.emoji).toBeNull
+    test('props from no-meta.md', () => {
+      let props = noMetaContent.props
+      expect(props.meta).toEqual({})
+      expect(props.html).toContain('<p>This is some markdown')
+      expect(props.title).toBeUndefined
+      expect(props.date).toBeUndefined
+      expect(props.emoji).toBeUndefined
+    })
   })
 })
