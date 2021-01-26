5bc4839cc4b56a258f4e8b86d2ca405b941d6723
Duncan Davidson <duncandavidson@me.com>
2021-01-26 23:03:47 +0100

    Cleaning up

--- lib/site.ts
+++ lib/site.ts
@@ -1,7 +1,6 @@
 import glob from 'glob'
-import path from 'path'
 import { Content, ContentProps } from './content'
-import * as _ from 'lodash'
+import * as lodash from 'lodash'
 
 export class Site {
   private _dir: string
@@ -50,7 +49,8 @@ export class Site {
   blogItems(n = 10): Array<Content> {
     // select all items that are of type link or post
 
-    return _.chain(Object.values(this.contents))
+    return lodash
+      .chain(Object.values(this.contents))
       .filter((o) => {
         return o.type == 'link' || o.type == 'post'
       })
@@ -78,45 +78,3 @@ export class Site {
     return site
   }
 }
-
-// export function contentPaths(dir = 'public'): Array<string> {
-//   return glob.sync(`${dir}/**/*.md`).map((value: string) => {
-//     return value.slice(dir.length + 1)
-//   })
-// }
-
-// export function staticPaths(dir = 'public'): Array<Array<string>> {
-//   return contentPaths(dir).map((value: string) => {
-//     return value.replace(/\.md$/, '').split('/')
-//   })
-// }
-
-// export function contentForStaticPath(
-//   staticPath: Array<string>,
-//   dir = 'public'
-// ): Content {
-//   let filename = path.join(dir, staticPath.join('/')) + '.md'
-//   let slug = '/' + staticPath.join('/')
-//   return new Content(filename, slug)
-// }
-
-// export function contentCollectionForStaticPrefix(
-//   staticPrefix: Array<string>,
-//   dir = 'public'
-// ): Array<Content> {
-//   let l = staticPrefix.length
-//   return _.chain(staticPaths(dir))
-//     .filter((o) => {
-//       return _.isEqual(staticPrefix, _.slice(o, 0, l))
-//     })
-//     .map((o) => {
-//       return contentForStaticPath(o, dir)
-//     })
-//     .sortBy([
-//       (o) => {
-//         return o.date
-//       },
-//     ])
-//     .reverse()
-//     .value()
-// }
