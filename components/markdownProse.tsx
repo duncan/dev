7edd37b63915996d7af9f7510e227085bd8c3e4c
Duncan Davidson <duncandavidson@me.com>
2021-02-03 22:19:46 +0100

    Prune unused code

--- components/markdownProse.tsx
+++ components/markdownProse.tsx
@@ -1,14 +1,5 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import { render } from 'react-dom'
-
-import unified from 'unified'
-import markdown from 'remark-parse'
-import html from 'remark-html'
-
-function markdownToHTML(text: string): string {
-  return unified().use(markdown).use(html).processSync(text).contents.toString()
-}
 
 export default function MarkdownProse({ text }: { text: string }) {
   return (
