fca5a4d29fd7e9cfe8f35ee92a4b7a138c8a29e1
Duncan Davidson <duncandavidson@me.com>
2021-02-03 16:21:48 +0100

    Start to refactor and clean up

--- components/textArticle.tsx
+++ components/textArticle.tsx
@@ -2,6 +2,27 @@ import Link from 'next/link'
 import { ContentProps } from '../lib/content'
 import MarkdownProse from './markdownProse'
 import DateLine from './dateLine'
+import { ReactElement } from 'react'
+
+function emojiDiv(content: ContentProps, linked = false): ReactElement {
+  let classes = ['text-5xl', 'pb-2']
+  if (linked) {
+    classes.push('cursor-pointer')
+  }
+  return <div className={classes.join(' ')}>{content.emoji}</div>
+}
+
+function titleDiv(content: ContentProps, linked = false): ReactElement {
+  let classes = ['text-4xl', 'font-bold']
+  if (linked) {
+    classes.push('cursor-pointer')
+  }
+  return <h1 className={classes.join(' ')}>{content.title}</h1>
+}
+
+function dateLineDiv(content: ContentProps, linked = false): ReactElement {
+  return content.date ? <DateLine date={content.date}></DateLine> : <></>
+}
 
 export default function TextArticle({
   content,
@@ -14,18 +35,19 @@ export default function TextArticle({
     <>
       <article className="container mx-auto max-w-xl pb-8 pt-16 px-4">
         <header>
-          <Link href={content.slug}>
-            <div className="text-5xl pb-2 cursor-pointer">{content.emoji}</div>
-          </Link>
-
-          <Link href={content.slug}>
-            <h1 className="text-4xl font-bold cursor-pointer">
-              {content.title}
-            </h1>
-          </Link>
-          <Link href={content.slug}>
-            <DateLine date={content.date}></DateLine>
-          </Link>
+          {home ? (
+            <>
+              <Link href={content.slug}>{emojiDiv(content, true)}</Link>
+              <Link href={content.slug}>{titleDiv(content, true)}</Link>
+              <Link href={content.slug}>{dateLineDiv(content, true)}</Link>
+            </>
+          ) : (
+            <>
+              {emojiDiv(content)}
+              {titleDiv(content)}
+              {dateLineDiv(content)}
+            </>
+          )}
         </header>
         <MarkdownProse text={content.text}></MarkdownProse>
       </article>
