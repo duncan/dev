dfa738c9607014b78990b0d4a5e4653bc5fe0fc1
Duncan Davidson <duncandavidson@me.com>
2021-02-06 15:15:52 +0100

    Add sitemap

---
+++ scripts/sitemap.ts
@@ -0,0 +1,33 @@
+import { Content } from '../lib/content'
+import { Site } from '../lib/site'
+import xml from 'xml'
+import fs from 'fs'
+
+//export function generate() {
+let contents = Site.instance().contents as { [slug: string]: Content }
+
+let urls = [
+  { _attr: { xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' } },
+] as {}[]
+let now = new Date().toISOString()
+urls.push({ url: [{ loc: 'https://duncan.dev/' }, { lastmod: now }] })
+
+Object.values(contents).forEach((content: Content) => {
+  let mtime = new Date(content.mtime).toISOString()
+  urls.push({
+    url: [{ loc: `https://duncan.dev/${content.slug}` }, { lastmod: mtime }],
+  })
+})
+
+let data = xml(
+  {
+    urlset: urls,
+  },
+  { indent: true }
+)
+console.log(data)
+
+fs.writeFileSync('./public/sitemap.xml', data)
+//}
+
+//generate()
