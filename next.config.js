0e4d84ee56c513ff8becace2b3d5d49d57321921
Duncan Davidson <duncandavidson@me.com>
2021-03-14 19:05:42 +0100

    Moving over old content

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/about',
-        destination: '/foo',
+        source: '/posts/2020/10/working',
+        destination: '/software/careers/how-do-i-work-best',
         permanent: true,
-      }
+      },
     ]
   },
   images: {
     deviceSizes: [320, 420, 640, 840, 1280],
     imageSizes: [320, 420, 640, 840, 1280],
-  }
+  },
 }
